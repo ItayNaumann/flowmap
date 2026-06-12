08f1200761bad757a740803c1fcd4570b66aac56
ItayNau <itay.naumann@gmail.com>
2026-06-12 18:24:57 +0300

    Add pattern match function to node type

--- apps/server/Server.Common/Models/NodeType.cs
+++ apps/server/Server.Common/Models/NodeType.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Server.Common.Models;
 
 public enum NodeType
@@ -5,4 +7,20 @@ public enum NodeType
 	Package,
 	Dashboard,
 	GiliTable
+}
+
+public static class NodeTypeExtensions
+{
+	public static bool IsIdPatternMatch(this NodeType type, string input)
+	{
+		string pattern = type switch
+		{
+			NodeType.Package => @"^[1-9]\d*$",
+			NodeType.Dashboard => @"^[0-9a-f]{14}$",
+			NodeType.GiliTable => @"[\S]*",
+			_ => throw new ArgumentOutOfRangeException(nameof(type), "Unknown content type")
+		};
+
+		return Regex.IsMatch(input, pattern);
+	}
 }
