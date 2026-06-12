24df92ed489d69e72b29e6d2d9fb36447ad329e7
ItayNau <itay.naumann@gmail.com>
2026-06-12 17:16:48 +0300

    Created identifier strategy class

---
+++ apps/server/Server.Common/IdentifierStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Server.Common.Models;
+
+namespace Server.Common;
+
+public class IdentifierStrategy(
+	Func<string, Node> getNodeData,
+	Func<string, IEnumerable<string>> getConnectedNodeIdentifiers)
+{
+	public Func<string, Node> GetNodeData = getNodeData;
+	public Func<string, IEnumerable<string>> GetConnectedNodeIdentifiers = getConnectedNodeIdentifiers;
+}
